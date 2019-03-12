95c7e2b0ecd9fe3f02f6e148707df5c8e7e9c52d
Haneyume <haneyume@gmail.com>
2019-03-13 03:15:32 +0800

    0.0.2

---
+++ docs/precache-manifest.8d03a4341ad760554c7d95ea25402ac9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9875e7fdcbcbbfee9db",
+    "url": "/static/css/main.e962e425.chunk.css"
+  },
+  {
+    "revision": "c9875e7fdcbcbbfee9db",
+    "url": "/static/js/main.fc30b0a6.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c79a1c326f9fcdf1c681",
+    "url": "/static/js/2.6946f263.chunk.js"
+  },
+  {
+    "revision": "54eddd668e04246ae09929e3b8e6e42a",
+    "url": "/index.html"
+  }
+];
